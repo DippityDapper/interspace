dfbae221f1d77c18f9061c737c00108ab25cecd2
DippityDapper <dustinchapline@gmail.com>
2025-11-07 14:34:04 -0500

    fixed scene issue

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char* argv[])
 {
     std::unique_ptr<Engine::Engine> engine = std::make_unique<Engine::Engine>();
-    std::unique_ptr<Game::WorldCreationScene> mainScene = std::make_unique<Game::WorldCreationScene>();
-    return engine->Run(mainScene.get());
+    Game::WorldCreationScene* mainScene = new Game::WorldCreationScene();
+    return engine->Run(mainScene);
 }
